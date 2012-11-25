f59fac79017c0397b54a9c13b95fd962432cf488
mahbube <mesn87@gmail.com>
2012-11-26 00:09:09 +0330

    personal website

---
+++ js/script.js
@@ -0,0 +1,84 @@
+// Zepto/jQuery fadeLoop plugin for fade slide show effects by ali.md
+(function($){
+	$.extend($.fn,{
+		fadeLoop :function(options){
+
+			options=$.extend({
+				duration : 2500,
+				delay : 4000,
+				startIndex : -1,
+				fadeFirstImage : true
+			},options);
+
+			var nextPic,
+				pics    =this,
+				indx    =options.startIndex,
+				plen    =this.length,
+				fadeIn  ={opacity:1},
+				fadeOut ={opacity:0},
+				ease = !!window.Zepto ? 'ease-in-out' : 'swing';
+
+			pics.css(fadeOut);
+			options.fadeFirstImage || pics.eq(0).css(fadeIn);
+
+			(nextPic=function(){
+				pics.eq(indx).animate(fadeOut,options.duration,ease);
+				indx=indx<plen-1?indx+1:0;
+				pics.eq(indx).animate(fadeIn,options.duration,ease,function(){
+					setTimeout(nextPic,options.delay);
+				});
+			})();
+		}
+	});
+})($)
+
+
+$(function(){
+	$('.backimg > div').fadeLoop();
+
+	$('.content_plc > div').animate({
+			right : 0 ,
+			top: '70px' ,
+	},1000,'swing')
+
+	//gallery
+	$('.pic_plc').animate({
+		right : 0
+	},1000,'swing')
+	// $('.gallery a>div').animate({
+	// 	height: '200px'
+	// },2000,'swing')
+});
+function showContent(div){
+	//$('#'+div).css({'right':'0px'});
+	// $('#contact').animate({
+	// 	right: '-650px' ,
+	// },1000,'swing',function(){
+	// 	$('#'+div).animate({
+	// 		right : 0 
+	// 	},1000,'swing')
+
+	// })
+	$('.content > div').animate({
+		right: '-650px' ,
+	},1000,'swing');
+	$('#'+div).animate({
+			right : 0 
+	},1000,'swing')
+	
+}
+function playVideo(videoName){
+	//$('#myfilm').attr('src' ,'videos/'+videoName);
+	//alert($('#myfilm').attr('src'));
+	$('#'+videoName).css({
+		zIndex: 1 ,
+	})
+	$('.list_film').animate({
+		right: '1500px' ,
+	},2000,'swing');
+
+	$('.show_film').animate({
+		right: '0px',
+	},2000,'swing')
+	
+}
